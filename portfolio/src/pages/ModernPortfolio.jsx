49cf58992ef1b59d515bc1ca8d4a3cf403d74767
NITESHBHARDWAJ001 <169159672+NITESHBHARDWAJ001@users.noreply.github.com>
2025-05-22 17:59:37 +0530

    Update ModernPortfolio.jsx

--- portfolio/src/pages/ModernPortfolio.jsx
+++ portfolio/src/pages/ModernPortfolio.jsx
@@ -254,7 +254,7 @@ export default function ModernPortfolio() {
                       <div className="absolute w-full h-full rounded-full bg-gradient-to-r from-indigo-500 to-purple-500 p-[3px]">
                         <div className="bg-white w-full h-full rounded-full overflow-hidden">
                           <img 
-                            src="./src/assets/WhatsApp Image 2025-05-22 at 17.09.25_a9fa148f.jpg" 
+                            src="https://res.cloudinary.com/dkhhwbixm/image/upload/v1747916901/WhatsApp_Image_2025-05-22_at_17.09.25_a9fa148f_dptvoj.jpg" 
                             alt="Nitesh Sharma" 
                             className="w-full h-full object-cover object-[40%_20%]"
                           />
